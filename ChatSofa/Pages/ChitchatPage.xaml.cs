276a700f0ec8966a317f56edcedf7e3513620083
201910 <201910@1220-006-22>
2023-02-09 18:53:12 +0300

    Добавьте файлы проекта.

---
+++ ChatSofa/Pages/ChitchatPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatSofa.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChitchatPage.xaml
+    /// </summary>
+    public partial class ChitchatPage : Page
+    {
+        public ChitchatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void finderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void closeApplicattionBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
